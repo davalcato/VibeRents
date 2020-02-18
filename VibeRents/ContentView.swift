33461bbc092f175fc0df7a3c681628c99082b7a9
davalcato <davalcato@yahoo.com>
2020-02-17 19:40:55 -0800

    added the .padding(.leading, 16).padding(.top, 16) for the burger Image underneath the Text username.

--- VibeRents/ContentView.swift
+++ VibeRents/ContentView.swift
@@ -54,8 +54,8 @@ struct PostView: View {
                     .frame(width: 60, height: 70)
                     .clipped()
                 Text("Username").font(.headline)
-                
-            }
+            }.padding(.leading, 16).padding(.top, 16)
+            
             Text("Post body text that will hopefully suport auto sizing vertically and span mutliple line").lineLimit(nil)
             Image("post_puppy")
                 .resizable()
